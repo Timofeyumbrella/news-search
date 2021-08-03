13e9177d5dd81f2aba80e7e6d5420ab3cf4b7a42
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-08-03 13:26:19 +0300

    add infinite scroll & define homepage logic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import InifiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+import { getNews, getMoreNews } from "../../redux/news/news.actions";
+
+import Article from "../../components/Article/Article";
+import Spinner from "../../components/Spinner/Spinner";
+import Error from "../../components/Error/Error";
+
+import "./Home.styles.scss";
 
 const Home = () => {
-  return <div className="home"></div>;
+  const [page, setPage] = useState(2);
+
+  const { loading, news, error } = useSelector((state) => state.news);
+  const { searchLoading, searchedNews, searchError } = useSelector(
+    (state) => state.searchedNews
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getNews());
+  }, [dispatch]);
+
+  const fetchMoreNews = async () => {
+    try {
+      setPage(page + 1);
+
+      const res = await axios.get(
+        `https://newsapi.org/v2/everything?q=all&sortBy=publishedAt&page=${page}&pageSize=5&apiKey=${process.env.REACT_APP_KEY}`
+      );
+
+      dispatch(
+        getMoreNews(
+          res.data.articles.map((article) => ({ id: uuidv4(), ...article }))
+        )
+      );
+    } catch (error) {
+      <Error error={error} />;
+    }
+  };
+
+  return (
+    <div className="home">
+      {loading || searchLoading ? (
+        <div className="home__loading">
+          <Spinner />
+        </div>
+      ) : searchedNews.length ? (
+        searchedNews.map((article) => {
+          return <Article key={article.id} article={article} />;
+        })
+      ) : news.length ? (
+        <InifiniteScroll
+          dataLength={news.length}
+          next={fetchMoreNews}
+          hasMore={page <= 20}
+        >
+          {news.map((article) => {
+            return <Article key={article.id} article={article} />;
+          })}
+        </InifiniteScroll>
+      ) : error || searchError ? (
+        <Error error={error} />
+      ) : null}
+    </div>
+  );
 };
 
 export default Home;
