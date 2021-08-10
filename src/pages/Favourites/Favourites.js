d38b2d96a67f468e8f5079142c204d65af78c6c3
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-08-10 15:53:30 +0300

    implement search through favourites

--- src/pages/Favourites/Favourites.js
+++ src/pages/Favourites/Favourites.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import Article from "components/Article/Article";
@@ -7,14 +7,35 @@ import "./Favourites.styles.scss";
 
 const Favourites = () => {
   const { favourites } = useSelector((state) => state.favourites);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const filteredFavourites = favourites.filter((favourite) =>
+    favourite.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="favourites">
-      {favourites.length ? (
-        favourites.map((article) => {
+      <input
+        type="search"
+        value={search}
+        placeholder="search through your favourites"
+        onChange={handleChange}
+        className="favourites__search"
+      />
+      {search.length > 0 && (
+        <h2 className="favourites__search-results">
+          {filteredFavourites.length} search results for: <span>{search}</span>
+        </h2>
+      )}
+      {filteredFavourites.length > 0 &&
+        filteredFavourites.map((article) => {
           return <Article key={article.id} article={article} />;
-        })
-      ) : (
+        })}
+      {!favourites.length && (
         <h2 className="favourites__empty">
           You don't have any favourite news articles yet
         </h2>
